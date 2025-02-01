df6b72b9d42d88adc6a7dade0966eb186a95dab7
syahril343 <syahril.21058@mhs.unesa.ac.id>
2025-02-01 16:57:04 +0700

    HeaderComponent

--- src/components/HeaderComponent.jsx
+++ src/components/HeaderComponent.jsx
@@ -2,11 +2,11 @@ import { FaUserCircle } from "react-icons/fa";
 
 const HeaderComponent = () => {
   return (
-    <div className="flex items-center justify-around py-8 px-4">
-      <div className="mr-[20rem]">
+    <div className="flex items-center justify-around py-8 px-4 bg-slate-300">
+      <div className="mr-[20rem] fixed mr-[67rem]">
         <h3 className="font-semibold text-3xl">Dashboard</h3>
       </div>
-      <div className="flex items-center gap-2 -mr-[20rem]">
+      <div className="flex items-center gap-2 ml-[95rem] fixed">
         <div className="text-2xl"><FaUserCircle /></div>
         <h4 className="font-semibold">Im Ziida</h4>
       </div>
